0542735c37b07217f551d30bd780362dc6ff6a50
Michael Rasolo <rasolon.michael@gmail.com>
2023-07-26 22:24:31 +0200

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,9 @@ function App({ Component, pageProps }) {
     <Provider store={store}>
       <Head>
         <title>Listopia - la wishlist pour les futurs darons</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png"/>
       </Head>
       <Navbar />
       <Component {...pageProps} />
